c6199bda75fffc7f35afe0693ef7d4eb0a29a7e2
Jinwoo Baek <baek32jinoo@gmail.com>
2019-07-08 21:35:16 -0700

    Add connect and schema

---
+++ connect_and_schema.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+set -o
+set -x
+
+rm cp-helm-charts-0.1.1.tgz
+helm package cp-helm-charts/
+helm install --set cp-schema-registry.enabled=true,cp-kafka-rest.enabled=false,cp-kafka-connect.enabled=true,cp-kafka.enabled=false,cp-zookeeper.enabled=false,cp-ksql-server.enabled=false,cp-control-center.enabled=false cp-helm-charts-0.1.1.tgz --tls --namespace other-kafka
+
